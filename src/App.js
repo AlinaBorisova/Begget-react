0088821f9d6203a52cc99bf388f426dff2c9315c
Алина Борисова <b-alina1999.borisova@yandex.ru>
2023-12-19 22:47:27 +0300

    formComment with Redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import {store} from './store';
+import {Provider} from 'react-redux';
 import {TokenContextProvider} from './context/tokenContext';
 import {AuthContextProvider} from './context/authContext';
 
 function App() {
   console.log('app');
   return (
-    <TokenContextProvider>
-      <AuthContextProvider>
-        <Header />
-        <Main />
-      </AuthContextProvider>
-    </TokenContextProvider>
+    <Provider store={store}>
+      <TokenContextProvider>
+        <AuthContextProvider>
+          <Header />
+          <Main />
+        </AuthContextProvider>
+      </TokenContextProvider>
+    </Provider>
   );
 }
 
--- src/components/Modal/FormComment/FormComment.jsx
+++ src/components/Modal/FormComment/FormComment.jsx
@@ -1,25 +1,27 @@
 import style from './FormComment.module.css';
 import {Text} from '../../../UI/Text';
-import {useContext, useRef, useState} from 'react';
+import {useContext, useState} from 'react';
 import {authContext} from '../../../context/authContext';
+import {useDispatch, useSelector} from 'react-redux';
+import {updateComment} from '../../../store';
 
 export const FormComment = () => {
-  const textareaRef = useRef(null);
+  const value = useSelector(state => state.comment);
+  const dispatch = useDispatch();
   const {auth} = useContext(authContext);
   const [isFormCommentsOpen, setFormCommentsOpen] = useState(false);
 
   const handleOpen = () => {
     setFormCommentsOpen(true);
-    if (textareaRef.current !== null) {
-      textareaRef.current.focus();
-    }
   };
 
   const handleSubmit = event => {
     event.preventDefault();
-    console.log(textareaRef.current.value);
-    textareaRef.current.value = null;
-    setFormCommentsOpen(false);
+    console.log(value);
+  };
+
+  const handleChange = event => {
+    dispatch(updateComment(event.target.value));
   };
 
   return (
@@ -29,7 +31,10 @@ export const FormComment = () => {
           <Text as='h3' size={14} tsize={18}>
             {auth.name}
           </Text>
-          <textarea className={style.textarea} ref={textareaRef}></textarea>
+          <textarea
+            className={style.textarea}
+            value={value}
+            onChange={handleChange} />
         </>
       }
       <button className={style.btn} onClick={
